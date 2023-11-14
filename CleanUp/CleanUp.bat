62421970233b6e105482c8bfd0575efd340c698c
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-11-14 11:24:42 +0700

    Update

--- CleanUp/CleanUp.bat
+++ CleanUp/CleanUp.bat
@@ -4,22 +4,44 @@ cd C:\Windows\Temp\
 del C:\Windows\Temp\* /q /s /f
 cd C:\Windows\Logs\
 del C:\Windows\Logs\* /q /s /f
-cd "C:\Users\Hendry Chandra\AppData\Local\Temp\"
-del "C:\Users\Hendry Chandra\AppData\Local\Temp\*" /q /s /f
+cd C:\Windows\debug\
+del C:\Windows\debug\* /q /s /f
 cd C:\Windows\Minidump\
 del C:\Windows\Minidump\* /q /s /f
+cd C:\Windows\Prefetch\
+del C:\Windows\Prefetch\* /q /s /f
+rem cd C:\Windows\SoftwareDistribution\
+rem del C:\Windows\SoftwareDistribution\* /q /s /f
+cd C:\ProgramData\Microsoft\Windows\WER\ReportArchive\
+del C:\ProgramData\Microsoft\Windows\WER\ReportArchive\* /q /s /f
+cd %userprofile%\AppData\Local\Temp\
+del %userprofile%\AppData\Local\Temp\* /q /s /f
+cd %userprofile%\AppData\Local\CrashDumps\
+del %userprofile%\AppData\Local\CrashDumps\* /q /s /f
 
+cd %userprofile%
 pause
 
-c:\windows\SYSTEM32\cleanmgr.exe /dC
+C:\windows\SYSTEM32\cleanmgr.exe /dC
 
 cd C:\Windows\Temp\
 del C:\Windows\Temp\* /q /s /f
 cd C:\Windows\Logs\
 del C:\Windows\Logs\* /q /s /f
-cd "C:\Users\Hendry Chandra\AppData\Local\Temp\"
-del "C:\Users\Hendry Chandra\AppData\Local\Temp\*" /q /s /f
+cd C:\Windows\debug\
+del C:\Windows\debug\* /q /s /f
 cd C:\Windows\Minidump\
 del C:\Windows\Minidump\* /q /s /f
+cd C:\Windows\Prefetch\
+del C:\Windows\Prefetch\* /q /s /f
+rem cd C:\Windows\SoftwareDistribution\
+rem del C:\Windows\SoftwareDistribution\* /q /s /f
+cd C:\ProgramData\Microsoft\Windows\WER\ReportArchive\
+del C:\ProgramData\Microsoft\Windows\WER\ReportArchive\* /q /s /f
+cd %userprofile%\AppData\Local\Temp\
+del %userprofile%\AppData\Local\Temp\* /q /s /f
+cd %userprofile%\AppData\Local\CrashDumps\
+del %userprofile%\AppData\Local\CrashDumps\* /q /s /f
 
+cd %userprofile%
 pause
